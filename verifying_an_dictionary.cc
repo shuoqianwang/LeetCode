a6eff8dee6f7492b63f080b7ae9560f77afca1a5
Yichen <30734635+yichen-git@users.noreply.github.com>
2020-11-08 13:17:36 -0500

    faster & smaller

--- verifying_an_dictionary.cc
+++ verifying_an_dictionary.cc
@@ -1,14 +1,14 @@
 class Solution {
 public:
-    bool checkOrder(string fst, string snd, unordered_map<char, int> umap) {
-        int flag = 0;
+    bool checkOrder(string fst, string snd, int* vec) {
+        int flag;
         for (int i = 0; i < min(fst.size(), snd.size()); i++) {
-            flag = umap.at(snd[i]) - umap.at(fst[i]);
-            if (flag == 0) {
+            flag = vec[snd[i] - 'a'] - vec[fst[i] - 'a'];
+            if (flag == 0)
                 continue;
-            } else if (flag > 0) {
+            else if (flag > 0)
                 break;
-            } else return false;
+            else return false;
         }
         if (flag == 0 && fst.size() > snd.size())
             return false;
@@ -16,14 +16,12 @@ public:
     }
 
     bool isAlienSorted(vector<string>& words, string order) {
-        unordered_map<char, int> umap;
-        for (int i = 0; i < order.size(); i++) {
-            umap[order[i]] = i;
-        }
-        for (auto it = words.begin(); it + 1 != words.end(); it++) {
-            if (!checkOrder(*it, *(it + 1), umap))
+        int vec[26] = {0};
+        for (int i = 0; i < order.size(); i++)
+            vec[order[i] - 'a'] = i;
+        for (auto it = words.begin(); it != words.end() - 1; it++)
+            if (!checkOrder(*it, *(it + 1), vec))
                 return false;
-        }
         return true;
     }
 };
