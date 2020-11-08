9051aeb9d395c75a6846e3a67678461cd29ac618
Yichen <30734635+yichen-git@users.noreply.github.com>
2020-11-08 17:43:11 -0500

    Update and rename reverse_linked_list.cc to 206_reverse_linked_list.cc

--- 206_reverse_linked_list.cc
+++ 206_reverse_linked_list.cc
@@ -3,9 +3,12 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
