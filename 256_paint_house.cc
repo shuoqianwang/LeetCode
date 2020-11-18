731f5e9339e6c82218ff023a45f6f6736010a866
Yichen <30734635+yichen-git@users.noreply.github.com>
2020-11-18 14:09:32 -0500

    DP

---
+++ 256_paint_house.cc
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int minCost(vector<vector<int>>& costs) {
+        if (costs.empty())
+            return 0;
+        for (int i = costs.size() - 2; i >= 0; --i) {
+            costs[i][0] += min(costs[i + 1][1], costs[i + 1][2]);
+            costs[i][1] += min(costs[i + 1][0], costs[i + 1][2]);
+            costs[i][2] += min(costs[i + 1][0], costs[i + 1][1]);
+        }
+        return min(costs[0][0], min(costs[0][1], costs[0][2]));
+    }
+};
